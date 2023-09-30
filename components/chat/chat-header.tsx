0cce20c2c67c0b0cea736c4adedbb74fc12ef43e
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-10-01 01:21:24 +0500

    Conversations Setup

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -22,7 +22,12 @@ export const ChatHeader: React.FC<ChatHeaderProps> = ({
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
-      
+      {type === "conversation" && (
+        <UserAvatar 
+          src={imageUrl}
+          className="h-8 w-8 md:h-8 md:w-8 mr-2"
+        />
+      )}
      
       <p className="font-semibold text-md text-black dark:text-white">
         {name}
