b7682b0383f9b34acec4f641d96827853b3f8e9d
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-10-07 01:14:28 +0500

    Chat Item added

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { Fragment } from "react";
+import { format } from "date-fns";
 import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
 
 import { ChatWelcome } from "./chat-welcome";
+import { ChatItem } from "./chat-item";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
@@ -26,14 +30,17 @@ interface ChatMessagesProps {
   type: "channel" | "conversation";
 }
 
-export const ChatMessages: React.FC<ChatMessagesProps> = ({
+export const ChatMessages = ({
   name,
+  member,
   chatId,
   apiUrl,
+  socketUrl,
+  socketQuery,
   paramKey,
   paramValue,
   type,
-}) => {
+}: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`;
 
   const {
@@ -70,21 +77,31 @@ export const ChatMessages: React.FC<ChatMessagesProps> = ({
 
   return (
     <div className="flex-1 flex flex-col py-4 overflow-y-auto">
-        <ChatWelcome
-          type={type}
-          name={name}
-        />
-        <div className="flex flex-col-reverse mt-auto">
-            {data?.pages?.map((group, i) => (
-            <Fragment key={i}>
-                {group.items.map((message: MessageWithMemberWithProfile) => (
-                <div key={message.id}>
-                    {message.content}
-                </div>
-                ))}
-            </Fragment>
+      <ChatWelcome
+        type={type}
+        name={name}
+      />
+      <div className="flex flex-col-reverse mt-auto">
+        {data?.pages?.map((group, i) => (
+          <Fragment key={i}>
+            {group.items.map((message: MessageWithMemberWithProfile) => (
+              <ChatItem
+                key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
-        </div>`
+          </Fragment>
+        ))}
+      </div>
     </div>
   )
 }
